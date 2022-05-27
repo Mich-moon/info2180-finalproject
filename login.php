bf6c46f6f606cc3e5b5b03b5a45e8390c2668ae3
Michaela Wright <75323745+Mich-moon@users.noreply.github.com>
2022-05-27 17:15:58 -0500

    database connection edits

--- login.php
+++ login.php
@@ -13,10 +13,12 @@ try {
 	$cleardb_username = $cleardb_url["user"];
 	$cleardb_password = $cleardb_url["pass"];
 	$cleardb_db = substr($cleardb_url["path"],1);
+
 	$active_group = 'default';
 	$query_builder = TRUE;
+	
 	// Connect to DB
-	$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+	$conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 	
 	//$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 } catch (Exception $e) {
